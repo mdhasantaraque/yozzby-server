5be01357ecd5cea9f14e8455f4da9807cc61569d
Md. Hasan Taraque <mtaraque32@gmail.com>
2022-12-29 18:00:37 +0600

    message details api called

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,11 +23,29 @@ async function run() {
     const messageCollection = client
       .db("yozzby")
       .collection("messageCollection");
+    const myAbout = client.db("yozzby").collection("about");
 
+    // get message
     app.get("/messageCollection", async (req, res) => {
       const query = {};
-      const users = await usersCollection.find(query).toArray();
-      res.send(users);
+      const messages = await messageCollection.find(query).toArray();
+      res.send(messages);
+    });
+    // get about me
+    app.get("/about", async (req, res) => {
+      const query = {};
+      const result = await myAbout.find(query).toArray();
+      // console.log(result);
+      res.send(result);
+    });
+
+    // messageDetails api sending by id
+
+    app.get("/messageDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await messageCollection.findOne(query);
+      res.send(result);
     });
     //   user posted text
     app.post("/messageCollection", async (req, res) => {
@@ -35,21 +53,31 @@ async function run() {
       const result = await messageCollection.insertOne(text);
       res.send(result);
     });
+
+    // updating about section
+
+    app.put("/about/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const user = req.body;
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          name: user.name,
+          email: user.email,
+          university: user.university,
+          address: user.address,
+        },
+      };
+      // console.log(updatedDoc);
+      const result = await myAbout.updateOne(filter, updatedDoc, option);
+      res.send(result);
+    });
   } finally {
   }
 }
 
 run().catch((error) => console.log(error));
-// const express = require("express");
-// const cors = require("cors");
-// const jwt = require("jsonwebtoken");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// require("dotenv").config();
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.send("Yozzby server is running");
